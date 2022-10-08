e1921b2f092f3b373eb4a48d00b5e87d51b87ce1
Francois <Fpaul1274@gmail.com>
2022-10-08 14:14:48 +0200

    updated windiCSS config

--- windi.config.js
+++ windi.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from "windicss/helpers";
 
 export default defineConfig({
   theme: {
+    extend: {
+      colors: {
+        purple: "#6E07F3",
+        green: "#5BE9B9",
+        navy: "#141c3a",
+      },
+      fontFamily: {
+        display: ["Oswald", "sans-serif"],
+        body: ["Nunito", "sans-serif"],
+      },
+    },
     container: {
       center: true,
       padding: {
